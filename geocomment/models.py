e9f38727bc0dd28b1aa2b4cdaa9cc1a462728b24
Christian Spanring <cspanring@gmail.com>
2011-05-10 11:08:11 -0400

    updated place model

--- geocomment/models.py
+++ geocomment/models.py
@@ -1,8 +1,29 @@
 from django.contrib.gis.db import models
+from django.forms import ModelForm, HiddenInput, TextInput
 
 # Create your models here.
 
+# workaround for South custom fields issues 
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ['^django\.contrib\.gis\.db\.models\.fields\.PointField'])
+except ImportError:
+    pass
+
+FOLLOWUP_CHOICES = (
+    ('n', 'New'),
+    ('v', 'Verification'),
+    ('c', 'Completed'),
+)
+
 class Place(models.Model):
+    
+    description = models.TextField()
+    
+    followup = models.CharField(max_length=1, default='n', choices=FOLLOWUP_CHOICES)
+    
+    ip = models.IPAddressField('IP Address', blank=True, null=True)
+    last_modified = models.DateTimeField(editable=False, auto_now=True)
     
     # GeoDjango
     location = models.PointField(geography=True, blank=True, null=True, default='POINT(0 0)') # default SRS 4326
@@ -10,3 +31,16 @@ class Place(models.Model):
     
     def __unicode__(self):
         return u'%s' % (self.id)
+    
+    def get_absolute_url(self):
+        return "/feedback/%i/" % self.id
+
+class PlaceForm(ModelForm):
+    
+    class Meta:
+        model = Place
+        exclude = ('ip', 'followup',)
+        widgets = {
+            'description': TextInput(),
+            'location': HiddenInput(),         
+        }
