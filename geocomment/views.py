7ba64796fd38db1b0f022d6716f30d8aefb5d537
Christian Spanring <cspanring@gmail.com>
2011-05-10 18:06:48 -0400

    basic save feedback functionality

--- geocomment/views.py
+++ geocomment/views.py
@@ -1,1 +1,36 @@
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+from django.utils.text import normalize_newlines
+
+from geocomment.models import Place, PlaceForm
+
 # Create your views here.
+
+def index(request):
+    
+    if request.method == 'POST':
+        form = PlaceForm(request.POST)
+        if form.is_valid():
+            entry = form.save(commit=False)
+            entry.ip = request.META['REMOTE_ADDR']  
+            # eliminate linebreaks
+            normalized_text = normalize_newlines(entry.description)            
+            entry.description = (normalized_text.replace('\n', ' '))     
+            entry.save()
+            return render_to_response('geocomment/thanks.html',
+                                      {'feedback': entry,},
+                                      context_instance=RequestContext(request))            
+    else:
+        form = PlaceForm()
+        
+    return render_to_response('geocomment/index.html', 
+                              {'form': form, 
+                               }, 
+                               context_instance=RequestContext(request))
+    
+def detail(request, feedback_id):
+    feedback = Place.objects.get(pk=feedback_id)    
+    return render_to_response('geocomment/detail.html',
+                            {'feedback': feedback,
+                             },
+                            context_instance=RequestContext(request))
--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,6 +9,14 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'trailmap1.views.home', name='home'),
     # url(r'^trailmap/', include('trailmap.foo.urls')),
+    # (r'^$', direct_to_template, {'template': 'index.html'}),
+    (r'^$', 'geocomment.views.index'),
+    
+    # add new feedback
+    (r'^feedback/new/', 'geocomment.views.index'),
+    
+    # feedback detail page with comment stream
+    (r'^feedback/(?P<feedback_id>\d+)/$', 'geocomment.views.detail'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
