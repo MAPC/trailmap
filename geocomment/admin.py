98ad53bbd0488a51077b911d030aad5560dadae9
Christian Spanring <cspanring@gmail.com>
2011-06-13 16:18:06 -0400

    added osm admin

--- geocomment/admin.py
+++ geocomment/admin.py
@@ -2,7 +2,7 @@ from trailmap.geocomment.models import Place
 # from django.contrib import admin
 from django.contrib.gis import admin
 
-class PlaceAdmin(admin.GeoModelAdmin):
+class PlaceAdmin(admin.OSMGeoAdmin):
     list_display = ('pk',)
 
 admin.site.register(Place, PlaceAdmin)
